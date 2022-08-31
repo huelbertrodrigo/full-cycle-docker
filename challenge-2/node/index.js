e86c6fb918fe16859965b12ad65435e6d4246234
Huelbert Rodrigo <rodrigo@scontti.com.br>
2022-08-31 14:36:20 -0300

    fix map person

--- challenge-2/node/index.js
+++ challenge-2/node/index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 3000
+
 const config = {
   host: 'db',
+  port: 3306,
+  database: 'nodedb',
   user: 'root',
-  password: 'root',
-  database: 'nodedb'
+  password: 'root'
 }
 
 const mysql = require('mysql')
@@ -14,13 +16,20 @@ const connection = mysql.createConnection(config)
 const sqlCreate = `create table if not exists people (id int not null auto_increment, name varchar(255), primary key(id));`
 connection.query(sqlCreate)
 
-const sqlInsert = `insert into people(name) values('John Joe');`
+const sqlInsert = `insert into people(name) values('Huelbert Rodrigo');`
 connection.query(sqlInsert)
+
+var html = '<h1>Full Cycle Rocks</h1>'
+const sqlSelect = `select * from people;`
+connection.query(sqlSelect, function (err, res) {
+  if (err) throw err
+  res.map(person => (html = html + '<p>' + person.name + '</p>'))
+})
 
 connection.end()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Full Cycle</h1>')
+  res.send(html)
 })
 
 app.listen(port, () => {
